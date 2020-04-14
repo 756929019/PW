2f1db75e8c2a32c6ab344199ff11460e573d3a6b
ab756929019 <756929019@qq.com>
2020-04-14 14:51:26 +0800

    no commit message

---
+++ Common/PW.Footer/FooterModule.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Modularity;
+using Prism.Mef.Modularity;
+using Prism.Regions;
+using System.ComponentModel.Composition;
+using System;
+using PW.Infrastructure;
+using PW.Common;
+
+namespace PW.Footer
+{
+    [ModuleExport(typeof(FooterModule), InitializationMode = InitializationMode.OnDemand)]
+    public class FooterModule : IModule
+    {
+        private readonly IModuleTracker moduleTracker;
+        private readonly IRegionManager regionManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModuleB"/> class.
+        /// </summary>
+        /// <param name="moduleTracker">The module tracker.</param>
+        [ImportingConstructor]
+        public FooterModule(IModuleTracker moduleTracker, IRegionManager regionManager)
+        {
+            if (moduleTracker == null)
+            {
+                throw new ArgumentNullException("moduleTracker");
+            }
+
+            this.moduleTracker = moduleTracker;
+            this.moduleTracker.RecordModuleConstructed(ModuleNames.Footer);
+            this.regionManager = regionManager;
+            CommandEvent cmdEvent = GlobalData.EventAggregator.GetEvent<CommandEvent>();
+            cmdEvent.Subscribe(OnCommandEvent);
+        }
+        private void OnCommandEvent(CommandEventArgs e)
+        {
+            Log.info("FooterModule OnCommandEvent");
+        }
+
+
+        /// <summary>
+        /// Notifies the module that it has be initialized.
+        /// </summary>
+        public void Initialize()
+        {
+            this.moduleTracker.RecordModuleInitialized(ModuleNames.Footer);
+            regionManager.RegisterViewWithRegion(RegionNames.Footer, typeof(FooterView));
+        }
+    }
+}
---
+++ Common/PW.Footer/FooterView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PW.Footer
+{
+    /// <summary>
+    /// FooterView.xaml 的交互逻辑
+    /// </summary>
+    [Export(typeof(FooterView))]
+    public partial class FooterView : UserControl
+    {
+        public FooterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
